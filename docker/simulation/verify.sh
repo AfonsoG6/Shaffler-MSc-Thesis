63f03fef14bb08afdab0f5354d367374429c2b62
Afonso Gomes <afonsopamg@gmail.com>
2023-03-09 18:13:39 +0000

    Simplified shadow simulation for our needs

--- docker/simulation/verify.sh
+++ docker/simulation/verify.sh
@@ -1,21 +1,22 @@
-#!/usr/bin/env bash
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-bootstrapped_count="$(grep -r --include="*.tor.*.stdout" "Bootstrapped 100" | wc -l)"
-printf "Bootstrapped count: ${bootstrapped_count}/9\n"
+EXPECTED_BOOTSTRAPPED_COUNT=6
+EXPECTED_TGEN_STREAMS=40
 
-if [ "${bootstrapped_count}" != "9" ]; then
+bootstrapped_count="$(grep -r --include="*.tor.*.stdout" "Bootstrapped 100" | wc -l)"
+printf "Bootstrapped count: ${bootstrapped_count}/$EXPECTED_BOOTSTRAPPED_COUNT\n"
+
+if [ "${bootstrapped_count}" != "$EXPECTED_BOOTSTRAPPED_COUNT" ]; then
     printf "Verification ${RED}failed${NC}: Not all tor processes bootstrapped :(\n"
     exit 1
 fi
 
 stream_count="$(grep -r --include="*.tgen.*.stdout" "stream-success" | wc -l)"
-printf "Successful tgen stream count: ${stream_count}/80\n"
+printf "Successful tgen stream count: ${stream_count}/$EXPECTED_TGEN_STREAMS\n"
 
-if [ "${stream_count}" != "80" ]; then
+if [ "${stream_count}" != "$EXPECTED_TGEN_STREAMS" ]; then
     printf "Verification ${RED}failed${NC}: Not all tgen streams were successful :(\n"
     exit 1
 fi
