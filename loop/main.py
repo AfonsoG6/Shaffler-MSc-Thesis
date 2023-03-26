54583233d5fdda7c5f4ed1285e74b431a8003537
Afonso Gomes <afonsopamg@gmail.com>
2023-03-26 03:28:15 +0100

    Added some prints

--- loop/main.py
+++ loop/main.py
@@ -9,4 +9,7 @@ if __name__ == '__main__':
     server_thread = Thread(target=run_server, args=[SERVER_PORT])
     server_thread.daemon = True
     server_thread.start()
+    print("Server started")
     run_client(SERVER_PORT, SOCKS_PORT)
+    print("Client finished")
+    print("Server closing")
