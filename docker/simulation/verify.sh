38f5dce03a36e6b64003b9b9d57935dc323da948
Afonso Gomes <afonsopamg@gmail.com>
2023-03-26 16:18:40 +0100

    Fixed verify script

--- docker/simulation/verify.sh
+++ docker/simulation/verify.sh
@@ -8,7 +8,7 @@ EXPECTED_TGEN_STREAMS=40
 bootstrapped_count="$(grep -r --include="*.tor.*.stdout" "Bootstrapped 100" | wc -l)"
 printf "Bootstrapped count: ${bootstrapped_count}/$EXPECTED_BOOTSTRAPPED_COUNT\n"
 
-if [ "${bootstrapped_count}" != "$EXPECTED_BOOTSTRAPPED_COUNT" ]; then
+if [ "${bootstrapped_count}" -lt "$EXPECTED_BOOTSTRAPPED_COUNT" ]; then
     printf "Verification ${RED}failed${NC}: Not all tor processes bootstrapped :(\n"
     exit 1
 fi
@@ -16,7 +16,7 @@ fi
 stream_count="$(grep -r --include="*.tgen.*.stdout" "stream-success" | wc -l)"
 printf "Successful tgen stream count: ${stream_count}/$EXPECTED_TGEN_STREAMS\n"
 
-if [ "${stream_count}" != "$EXPECTED_TGEN_STREAMS" ]; then
+if [ "${stream_count}" -lt "$EXPECTED_TGEN_STREAMS" ]; then
     printf "Verification ${RED}failed${NC}: Not all tgen streams were successful :(\n"
     exit 1
 fi
