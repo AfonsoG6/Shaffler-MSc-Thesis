c4009fc5ba459ba5ed7a05825d8b64722c1324b8
Afonso Gomes <afonsopamg@gmail.com>
2023-03-26 00:11:13 +0000

    Fixed simulation again (i hope)

--- docker/simulation/run.sh
+++ docker/simulation/run.sh
@@ -1,4 +1,5 @@
 rm -rf shadow.data shadow.log
+cp -r ~/rendezmix/loop/* shadow.data.template/hosts/torclient
 
 # Run the Tor minimal test and store output in shadow.log
 shadow --template-directory shadow.data.template tor-minimal.yaml > shadow.log
