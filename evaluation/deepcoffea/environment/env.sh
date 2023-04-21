3976b884d43e42af9b1bc9c51e89cdffc6ff96f1
Afonso Gomes <afonsopamg@gmail.com>
2023-04-21 17:25:25 +0100

    Scripts to create environment for deepcoffea Better interface for deepcoffea scripts

---
+++ evaluation/deepcoffea/environment/env.sh
@@ -0,0 +1,7 @@
+conda create -y --name tf python=3.6.8
+conda activate tf
+conda install -y cudatoolkit=10.0.130 cudnn=7.6.4=cuda10.0_0
+pip install -r requirements.txt
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
