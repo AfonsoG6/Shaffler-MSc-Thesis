685709946ab6458b272a63ba4bbe8288dba04e4e
Afonso Gomes <afonsopamg@gmail.com>
2023-04-22 16:17:17 +0100

    Fixed env script

--- evaluation/deepcoffea/environment/env.sh
+++ evaluation/deepcoffea/environment/env.sh
@@ -13,11 +13,13 @@ then
 fi
 
 conda create -y --name $ENV_NAME python=3.6.8
-conda run -n $ENV_NAME pip install -r requirements.txt
+eval "$(conda shell.bash hook)"
+conda activate $ENV_NAME
+pip install -r requirements.txt
 if [ $gpu -gt 0 ];
 then
-    conda run -n $ENV_NAME conda install -y cudatoolkit=10.0.130 cudnn=7.6.4=cuda10.0_0
-    conda run -n $ENV_NAME mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-    conda run -n $ENV_NAME echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-    conda run -n $ENV_NAME source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+    conda install -y cudatoolkit=10.0.130 cudnn=7.6.4=cuda10.0_0
+    mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+    source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 fi
