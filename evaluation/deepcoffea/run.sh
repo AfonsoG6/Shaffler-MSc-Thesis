5ba1c6eda6f48a026588b9af5f3c3c7cc2ac2b2a
Afonso Gomes <afonsopamg@gmail.com>
2023-04-24 17:47:19 +0100

    better run script

--- evaluation/deepcoffea/run.sh
+++ evaluation/deepcoffea/run.sh
@@ -1,6 +1,6 @@
 ENV_NAME="tf"
 
-while getopts t:i:w:a:g:r: flag
+while getopts t:i:w:a:g:r:m: flag
 do
     case "${flag}" in
         t) threshold=${OPTARG};;
@@ -9,6 +9,7 @@ do
         a) addnum=${OPTARG};;
         g) gpu=${OPTARG};;
         r) redirect=${OPTARG};;
+        m) mode=${OPTARG};;
     esac
 done
 
@@ -22,11 +23,24 @@ then
     redirect=0
 fi
 
-echo "threshold: $threshold | interval: $interval | windows: $windows | addnum: $addnum | use gpu: $gpu | redirect: $redirect"
+if [ -z "$mode" ];
+then
+    mode=1
+fi
+
+mode = $mode - 1
+
+echo "mode: $mode | threshold: $threshold | interval: $interval | windows: $windows | addnum: $addnum | use gpu: $gpu | redirect: $redirect"
 
 if [ -z "$interval" ] || [ -z "$windows" ] || [ -z "$addnum" ];
 then
     echo "Please enter all the arguments"
+    exit 1
+fi
+
+if [ $mode -eq 0 ] && [ -z "$threshold" ];
+then
+    echo "Please enter threshold"
     exit 1
 fi
 
@@ -38,21 +52,42 @@ mkdir -p datasets/new_dcf_data/
 eval "$(conda shell.bash hook)"
 conda activate $ENV_NAME
 
-if [ ! -z "$threshold" ];
+if [ $mode -lt 1 ];
 then
     if [ $redirect -gt 0 ];
     then
         python3 filter.py -t $threshold -i $interval -w $windows -a $addnum &> filter.txt
-        python3 new_dcf_parse.py -i $interval -w $windows -a $addnum &> parse.txt
     else
         python3 filter.py -t $threshold -i $interval -w $windows -a $addnum
+    fi
+fi
+
+if [ $mode -lt 2 ];
+then
+    if [ $redirect -gt 0 ];
+    then
+        python3 new_dcf_parse.py -i $interval -w $windows -a $addnum &> parse.txt
+    else
         python3 new_dcf_parse.py -i $interval -w $windows -a $addnum
     fi
 fi
 
-if [ $redirect -gt 0 ];
+if [ $mode -lt 3 ];
 then
-    python3 train_fens.py -i $interval -w $windows -a $addnum -g $gpu &> train.txt
-else
-    python3 train_fens.py -i $interval -w $windows -a $addnum -g $gpu
+    if [ $redirect -gt 0 ];
+    then
+        python3 train_fens.py -i $interval -w $windows -a $addnum -g $gpu &> train.txt
+    else
+        python3 train_fens.py -i $interval -w $windows -a $addnum -g $gpu
+    fi
+fi
+
+if [ $mode -lt 4 ];
+then
+    if [ $redirect -gt 0 ];
+    then
+        python3 eval_dcf.py -i $interval -w $windows -a $addnum -g $gpu &> eval.txt
+    else
+        python3 eval_dcf.py -i $interval -w $windows -a $addnum -g $gpu
+    fi
 fi
