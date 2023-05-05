dfaa84ab8b6e7679fc2206bfe65ba92cf55cb58f
Afonso Gomes <afonsopamg@gmail.com>
2023-05-05 18:45:48 +0100

    More restrictive pcap script

--- simulation/pcap_enabler.py
+++ simulation/pcap_enabler.py
@@ -2,8 +2,11 @@ import yaml
 from argparse import ArgumentParser
 
 
-def enable_pcap(hosts: dict):
+def enable_pcap(hosts: dict, hostnames: list[str] = []):
     for host in hosts.keys():
+        if len(hostnames) > 0 and host not in hostnames:
+            continue
+
         host_config = hosts[host]
 
         if "options" in host_config.keys():
@@ -15,12 +18,14 @@ def enable_pcap(hosts: dict):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-f", "--file", type=str, required=True)
+    parser.add_argument("-h", "--hosts", type=str, required=False)
 
     args = parser.parse_args()
     filename: str = args.file
+    hostnames: list[str] = args.hosts.split(",") if args.hosts else []
 
     config = yaml.load(open(filename, "r"), Loader=yaml.FullLoader)
-    enable_pcap(config["hosts"])
+    enable_pcap(config["hosts"], hostnames)
     print("PCAP traces enabled")
 
     yaml.dump(config, open(filename, "w"),
