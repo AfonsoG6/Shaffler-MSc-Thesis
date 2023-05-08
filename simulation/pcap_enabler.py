ad78391134e46e3dc381e8dba8a401c5e6b4bb6e
Afonso Gomes <afonsopamg@gmail.com>
2023-05-08 16:31:32 +0100

    Better pcap enabler

--- simulation/pcap_enabler.py
+++ simulation/pcap_enabler.py
@@ -2,30 +2,34 @@ import yaml
 from argparse import ArgumentParser
 
 
-def enable_pcap(hosts: dict, hostnames: list[str] = []):
+def enable_pcap(hosts: dict, hostnames: list, max_packet_size: int):
     for host in hosts.keys():
         if len(hostnames) > 0 and host not in hostnames:
             continue
 
         host_config = hosts[host]
 
-        if "options" in host_config.keys():
-            host_config["options"]["pcap_directory"] = "."
-        else:
-            host_config["options"] = {"pcap_directory": "."}
+        if "options" not in host_config.keys():
+            host_config["options"] = {}
+        host_config["options"]["pcap_directory"] = "."
+        host_config["options"]["pcap_capture_size"] = f"{max_packet_size} B"
+        print(f"PCAP enabled for {host}")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-f", "--file", type=str, required=True)
     parser.add_argument("-h", "--hosts", type=str, required=False)
+    parser.add_argument("-s", "--max-packet-size",
+                        type=int, required=False, default=128)
 
     args = parser.parse_args()
     filename: str = args.file
-    hostnames: list[str] = args.hosts.split(",") if args.hosts else []
+    hostnames: list = args.hosts.split(",") if args.hosts else []
+    max_packet_size: int = args.max_packet_size
 
     config = yaml.load(open(filename, "r"), Loader=yaml.FullLoader)
-    enable_pcap(config["hosts"], hostnames)
+    enable_pcap(config["hosts"], hostnames, max_packet_size)
     print("PCAP traces enabled")
 
     yaml.dump(config, open(filename, "w"),
