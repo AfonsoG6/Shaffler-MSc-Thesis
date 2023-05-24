19e7a94669c13316ee24be624169da5dc7567a8c
Afonso Gomes <afonsopamg@gmail.com>
2023-05-24 19:05:27 +0100

    fixed setup script

--- vagrant/modified/setup.sh
+++ vagrant/modified/setup.sh
@@ -163,7 +163,7 @@ userstats-relay-country.csv \
 tmodel-ccs2018.github.io \
 --onionperf_data_path onionperf-2023-04 \
 --bandwidth_data_path bandwidth-2023-04.csv \
---geoip_path ~/tor/src/config/geoip"
+--geoip_path ~/rendezmix/tor/src/config/geoip"
 
 echo "alias stage=\"cd ~/rendezmix/simulation/data; tornettools stage \
 consensuses-2023-04 \
@@ -172,7 +172,7 @@ userstats-relay-country.csv \
 tmodel-ccs2018.github.io \
 --onionperf_data_path onionperf-2023-04 \
 --bandwidth_data_path bandwidth-2023-04.csv \
---geoip_path ~/tor/src/config/geoip\"" | tee -a ~/.profile
+--geoip_path ~/rendezmix/tor/src/config/geoip\"" | tee -a ~/.profile
 
 alias generate="cd ~/rendezmix/simulation; tornettools generate \
 data/relayinfo_staging_2023-04-01--2023-04-30.json \
