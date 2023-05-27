e21c65c6b322a823248a6de046bc7a183d95a2f2
Afonso Gomes <afonsopamg@gmail.com>
2023-05-27 20:20:52 +0100

    new patcher (customclient)

--- simulation/patch.py
+++ simulation/patch.py
@@ -5,54 +5,15 @@ import yaml
 import re
 import os
 
-
-def patch_clients(hosts: dict, hostnames: list, num_clients: int, max_packet_size: int) -> set:
-    global hosts_path, tgen_server_path, tgen_server_dir_path
-    pattern = re.compile(r"markovclient\d+exit")
-    ports = set()
-    
-    i: int = 0
-    host_keys = list(hosts.keys())
-    random.shuffle(host_keys)
-    for host in host_keys:
-        if pattern.match(host) and ((len(hostnames) > 0 and host in hostnames) or len(hostnames) == 0):
-            if num_clients > 0 and i >= num_clients:
-                break
-            i += 1
-
-            host_config = hosts[host]
-
-            if "host_options" not in host_config.keys():
-                host_config["host_options"] = {}
-            host_config["host_options"]["pcap_enabled"] = True
-            host_config["host_options"]["pcap_capture_size"] = f"{max_packet_size} B"
-            print(f"PCAP enabled for {host}")
-
-            host_config["processes"].append(
-                {"path": "hostname", "args": "-I", "start_time": 60})
-            print(f"Added hostname process to {host}")
-            
-            idx = int(host[len("markovclient"):-len("exit")])
-            print(f"Markov client {idx}")
-            port = 10000 + idx
-            ports.add(port)
-            
-            patch_client_tgenrc(port, os.path.join(hosts_path, host, "tgenrc.graphml"))
-            print(f"Replaced tgenrc for {host} (path: {os.path.join(hosts_path, host, 'tgenrc.graphml')})")
-            patch_server_tgenrc(port, tgen_server_path, os.path.join(tgen_server_dir_path, f"{port}.tgenrc.graphml"))
-            print(f"Created duplicate tgenrc for {host} with port {port}")
-    return ports
-
-
-def patch_servers(hosts: dict, hostnames: list, max_packet_size: int, ports: set):
+def patch_servers(hosts: dict, ports: set):
     pattern = re.compile(r"server\d+exit")
     for host in hosts.keys():
-        if pattern.match(host) and ((len(hostnames) > 0 and host in hostnames) or len(hostnames) == 0):
+        if pattern.match(host):
             host_config = hosts[host]
             if "host_options" not in host_config.keys():
                 host_config["host_options"] = {}
             host_config["host_options"]["pcap_enabled"] = True
-            host_config["host_options"]["pcap_capture_size"] = f"{max_packet_size} B"
+            host_config["host_options"]["pcap_capture_size"] = f"24 B"
             print(f"PCAP enabled for {host}")
             
             host_config["processes"].append(
@@ -65,14 +26,40 @@ def patch_servers(hosts: dict, hostnames: list, max_packet_size: int, ports: set
                 host_config["processes"].append(process)
             print("Added duplicate tgen processes to server")
 
-def patch_client_tgenrc(new_port: int, original_path: str, target_path: str = ""):
-    if target_path == "":
-        target_path = original_path
-    with open(original_path, "r") as f:
-        tgenrc = f.read()
-    tgenrc = tgenrc.replace(":80", f":{new_port}")
-    with open(target_path, "w") as f:
-        f.write(tgenrc)
+def netnodeid_ok(hosts: dict, netnodeid: int):
+    for host in hosts.keys():
+        if hosts[host]["network_node_id"] == netnodeid:
+            return False
+    return True
+
+def create_client(hosts: dict, idx: int):
+    global hosts_path, tgen_server_path, tgen_server_dir_path
+    
+    newhostname: str = f"customclient{idx}"
+    port: int = 10000 + idx
+    # Copy customclient directory to hosts_path
+    templates_path = "templates"
+    dir_path = os.path.join(templates_path, "customclient")
+    client_path = os.path.join(hosts_path, newhostname)
+    os.makedirs(client_path, exist_ok=True)
+    for file in os.listdir(dir_path):
+        with open(os.path.join(dir_path, file), "r") as f:
+            with open(os.path.join(client_path, file), "w") as g:
+                data = f.read()
+                if file == "tgenrc.graphml":
+                    data = data.replace("{port}", str(port))
+                g.write(data)
+    print(f"Created {newhostname} directory")
+    config_path = os.path.join(templates_path, "customclient.yaml")
+    new_host = yaml.load(open(config_path, "r"), Loader=yaml.FullLoader)
+    netnodeid: int = random.randint(0, 2520)
+    while not netnodeid_ok(hosts, netnodeid):
+        netnodeid = random.randint(0, 2520)
+    new_host["network_node_id"] = netnodeid
+    hosts[newhostname] = new_host
+    print(f"Added {newhostname} to shadow.config.yaml")
+    patch_server_tgenrc(port, tgen_server_path, os.path.join(tgen_server_dir_path, f"{port}.tgenrc.graphml"))
+    print(f"Created duplicate tgenrc for {newhostname} with port {port}")
 
 def patch_server_tgenrc(new_port: int, original_path: str, target_path: str = ""):
     if target_path == "":
@@ -87,16 +74,12 @@ def patch_server_tgenrc(new_port: int, original_path: str, target_path: str = ""
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-s", "--simulation", type=str, required=True)
-    parser.add_argument("-n", "--hostnames", type=str, required=False)
     parser.add_argument("-c", "--num-clients", type=int, required=False, default=-1)
-    parser.add_argument("-p", "--max-packet-size", type=int, required=False, default=24)
     parser.add_argument("-d", "--duration", type=float, required=False, default=1)
 
     args = parser.parse_args()
     simulation: str = args.simulation
-    hostnames: list = args.hostnames.split(",") if args.hostnames else []
     num_clients: int = args.num_clients
-    max_packet_size: int = args.max_packet_size
     duration: float = args.duration
     
     config_path = os.path.join(simulation, "shadow.config.yaml")
@@ -109,8 +92,11 @@ if __name__ == "__main__":
 
     config = yaml.load(open(config_path, "r"), Loader=yaml.FullLoader)
     config["general"]["stop_time"] = int(duration * 3600)
-    ports = patch_clients(config["hosts"], hostnames, num_clients, max_packet_size)
-    patch_servers(config["hosts"], hostnames, max_packet_size, ports)
+    ports: set = set()
+    for idx in range(num_clients):
+        create_client(config["hosts"], idx)
+        ports.add(10000 + idx)
+    patch_servers(config["hosts"], ports)
     
     yaml.dump(config, open(config_path, "w"), default_flow_style=False, sort_keys=False)
 
