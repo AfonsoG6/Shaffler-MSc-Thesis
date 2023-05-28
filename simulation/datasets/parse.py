b69d27d3292dd9fcf8aaff35b9c3837a9e98fb0a
Afonso Gomes <afonsopamg@gmail.com>
2023-05-28 14:03:18 +0100

    added capture interval to parse.py

--- simulation/datasets/parse.py
+++ simulation/datasets/parse.py
@@ -8,11 +8,13 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("-s", "--simulation", type=str, required=True)
     parser.add_argument("-o", "--output_path", type=str, required=False, default="dataset")
+    parser.add_argument("-c", "--capture_interval", type=float, default=150)
     args = parser.parse_args()
     
     # Parse arguments
     simulation: str = args.simulation
     output_path: str = args.output_path
+    capture_interval: float = args.capture_interval
     stage_path: str = "stage"
     
     if not os.path.exists(simulation):
@@ -37,6 +39,7 @@ def main():
         processes.append(Popen(["python3", "parse_host.py",
             "-s", simulation,
             "-n", hostname,
+            "-c", str(capture_interval),
             "-o", output_path]))
     for process in processes:
         process.wait()
