92c59c2844d6b6b7afb6a8670e4fb768a45cc37f
Afonso Gomes <afonsopamg@gmail.com>
2023-05-29 15:51:15 +0100

    added ETA

--- simulation/monitor.py
+++ simulation/monitor.py
@@ -17,19 +17,23 @@ def read_last_line(filename: str):
         return line
 
 def get_simulation_time(logfile: str):
-    return convert_time(read_last_line(logfile).split(" ")[2])
+    return dtstr_to_sec(read_last_line(logfile).split(" ")[2])
 
-def convert_time(time_str: str):
+def dtstr_to_sec(time_str: str):
     if len(time_str) > 15:
         time_str = time_str[:15]
     time_obj = datetime.strptime(time_str, "%H:%M:%S.%f")
     duration = timedelta(hours=time_obj.hour, minutes=time_obj.minute, seconds=time_obj.second, microseconds=time_obj.microsecond)
     return duration.total_seconds()
 
+def sec_to_dtstr(time_sec: float):
+    return datetime.strptime(str(time_sec), "%S.%f").strftime("%H:%M:%S.%f")
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("-s", "--simulation", type=str, required=True)
     parser.add_argument("-i", "--interval", type=float, required=False, default=30)
+    parser.add_argument("-d", "--duration", type=float, default=3)
     args = parser.parse_args()
     
     logfile = os.path.join(args.simulation, "shadow.log")
@@ -41,7 +45,8 @@ def main():
         time.sleep(args.interval)
         t2 = get_simulation_time(logfile)
         curr_speed = round((t2-t1)/args.interval, 6)
-        print(f"Previous SPD: {'{:.6f}'.format(prev_speed)}s /s     Current SPD: {'{:.6f}'.format(curr_speed)}s /s", end="\r")
+        eta = sec_to_dtstr((args.duration*3600-t2)/curr_speed)
+        print(f"Previous SPD: {'{:.6f}'.format(prev_speed)}s /s   Current SPD: {'{:.6f}'.format(curr_speed)}s /s   ETA: {eta}......", end="\r")
         prev_speed = curr_speed
 
 if __name__ == "__main__":
