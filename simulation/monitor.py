1bade35161af9ad874c81888b71bd8dbfcb75828
Afonso Gomes <afonsopamg@gmail.com>
2023-05-29 20:21:23 +0100

    use avg speed for eta

--- simulation/monitor.py
+++ simulation/monitor.py
@@ -6,18 +6,20 @@ from argparse import ArgumentParser
 def read_last_line(filename: str):
     with open(filename, "rb") as file:
         file.seek(-2, os.SEEK_END)
-        while file.read(1) != b'\n':
+        while file.read(1) != b"\n":
             file.seek(-2, os.SEEK_CUR)
         line = file.readline().decode()
         while "shadow-worker" not in line:
-            file.seek(-len(line)-2, os.SEEK_CUR)
-            while file.read(1) != b'\n':
+            file.seek(-len(line) - 2, os.SEEK_CUR)
+            while file.read(1) != b"\n":
                 file.seek(-2, os.SEEK_CUR)
             line = file.readline().decode()
         return line
 
+
 def get_simulation_time(logfile: str):
     return dtstr_to_sec(read_last_line(logfile).split(" ")[2])
+
 
 def dtstr_to_sec(time_str: str):
     if len(time_str) > 15:
@@ -26,11 +28,13 @@ def dtstr_to_sec(time_str: str):
     duration = timedelta(hours=time_obj.hour, minutes=time_obj.minute, seconds=time_obj.second, microseconds=time_obj.microsecond)
     return duration.total_seconds()
 
+
 def sec_to_dtstr(time_sec: float):
     hours = int(time_sec // 3600)
     minutes = int((time_sec % 3600) // 60)
     seconds = int(time_sec % 60)
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
 
 def main():
     parser = ArgumentParser()
@@ -38,22 +42,23 @@ def main():
     parser.add_argument("-i", "--interval", type=float, required=False, default=30)
     parser.add_argument("-d", "--duration", type=float, default=3)
     args = parser.parse_args()
-    
+
     logfile = os.path.join(args.simulation, "shadow.log")
-    
+
     avg_speed = 0
     t2 = get_simulation_time(logfile)
     while True:
         t1 = t2
         time.sleep(args.interval)
         t2 = get_simulation_time(logfile)
-        curr_speed = round((t2-t1)/args.interval, 6)
+        curr_speed = round((t2 - t1) / args.interval, 6)
         if avg_speed == 0:
             avg_speed = curr_speed
         else:
-            avg_speed = (avg_speed+curr_speed)/2
-        eta = sec_to_dtstr((args.duration*3600-t2)/curr_speed)
+            avg_speed = avg_speed*3/4 + curr_speed*1/4
+        eta = sec_to_dtstr((args.duration * 3600 - t2) / avg_speed)
         print(f"Average SPD: {'{:.6f}'.format(avg_speed)}s /s   Current SPD: {'{:.6f}'.format(curr_speed)}s /s   ETA: {eta}......", end="\r")
+
 
 if __name__ == "__main__":
     main()
