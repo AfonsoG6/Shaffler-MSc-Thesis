fe825cf0e9342d91c86e078f9d770962fcd758cd
Afonso Gomes <afonsopamg@gmail.com>
2023-06-13 00:33:56 +0100

    updated stage.py

--- simulation/datasets/stage.py
+++ simulation/datasets/stage.py
@@ -4,11 +4,8 @@ import json
 import os
 import re
 
-CLOCK_SYNC = -946684800
-
 info_clients: dict = {} # {client_name: [{timestamp, circuit_idx, site_idx}]}
 info_servers: list = [] # [{timestamp, port, circuit_idx, site_idx}]
-circuit_idxs: dict = {}
 site_counter: int = 0
 
 def find_clients(hosts_path: str) -> list:
@@ -24,53 +21,35 @@ def find_clients(hosts_path: str) -> list:
     return clients
 
 
-def parse_oniontrace(hostname: str, hosts_path: str) -> None:
+def get_client_flows(hostname: str, hosts_path: str) -> None:
     global info_clients, info_servers, site_counter
 
-    stream_new_pattern = re.compile(r"STREAM \d+ NEW")
-    oniontrace_path: str = os.path.join(hosts_path, hostname, "oniontrace.1002.stdout")
-    if not os.path.exists(oniontrace_path):
-        raise Exception(f"Oniontrace file not found for {hostname} in {hosts_path}")
+    host_path = os.path.join(hosts_path, hostname)
 
     if hostname not in info_clients.keys():
         info_clients[hostname] = []
     circuit_idx: int = int(hostname[len("customclient"):])
+    port: int = 10000 + circuit_idx
     
-    last_start_ts: float = -100000
-    
-    with open(oniontrace_path, "r") as file:
-        while True:
-            line: str = file.readline()
-            if len(line) == 0:
-                break
-            match = stream_new_pattern.search(line)
-            if match:
-                tokens: list = line[match.start():].split(" ")
-                site: str = tokens[4]
-                port: int = int(site.split(":")[1])
-                if "$" in site:
-                    continue
-                timestamp: float = round(float(line.split(" ")[2]) + CLOCK_SYNC, 6)
-                if timestamp < last_start_ts + 60:
-                    # Still the same flow
-                    continue
-                last_start_ts = timestamp
-                site_idx: int = site_counter
-                site_counter += 1
-                info_clients[hostname].append({
-                    "timestamp": timestamp,
-                    "duration": 30,
-                    "circuit_idx": circuit_idx,
-                    "site_idx": site_idx
-                })
-                info_servers.append({
-                    "timestamp": timestamp,
-                    "duration": 30,
-                    "port": port,
-                    "circuit_idx": circuit_idx,
-                    "site_idx": site_idx
-                })
-                print(f"Found flow for {hostname} at {timestamp}")
+    for file in os.listdir(host_path):
+        if file.endswith(".tgenrc.graphml"):
+            timestamp: int = int(file.split(".")[0].replace("t", ""))
+            site_idx: int = site_counter
+            site_counter += 1
+            info_clients[hostname].append({
+                "timestamp": timestamp,
+                "duration": 30,
+                "circuit_idx": circuit_idx,
+                "site_idx": site_idx
+            })
+            info_servers.append({
+                "timestamp": timestamp,
+                "duration": 30,
+                "port": port,
+                "circuit_idx": circuit_idx,
+                "site_idx": site_idx
+            })
+            print(f"Found flow for {hostname} at {timestamp}")
 
 def main():
     parser = ArgumentParser()
@@ -89,7 +68,7 @@ def main():
 
     clients: list = find_clients(hosts_path)
     for client in clients:
-        parse_oniontrace(client, hosts_path)
+        get_client_flows(client, hosts_path)
 
     with open(os.path.join("stage", f"info_clients.pickle"), "wb") as file:
         pickle.dump(info_clients, file)
