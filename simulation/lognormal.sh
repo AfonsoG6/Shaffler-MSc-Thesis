db1110c861a64a2f6b5f4d30f004889bd2dfb010
Afonso Gomes <afonso.gomes@tecnico.ulisboa.pt>
2023-07-06 01:06:58 +0100

    Added new shell scripts for simulations

---
+++ simulation/lognormal.sh
@@ -0,0 +1,21 @@
+# ----------------------- NORMAL SIMULATION ----------------------- #
+echo "Starting Lognormal Simulation"
+cd /root/rendezmix/tor
+git pull
+./autogen.sh
+./configure --disable-asciidoc --disable-unittests
+make
+make install
+
+cd /part/simulation
+rm -rf sim_300_2_l/shadow.data sim_300_2_l/shadow.log
+tornettools simulate sim_300_2_l
+zip -r Lognormal_sim.zip sim_300_2_l
+
+cd /part/simulation/datasets/
+python3 stage.py -s ../sim_300_2_l
+python3 parse.py -s ../sim_300_2_l -o Lognormal
+python3 cleanup.py -d Lognormal
+zip -r Lognormal_dataset.zip Lognormal
+rm -rf Lognormal
+echo "Done!"
--- simulation/prepare.sh
+++ simulation/prepare.sh
@@ -8,14 +8,30 @@ cp -r /root/rendezmix/simulation /part
 cd /part/simulation/data
 tornettools stage consensuses-2023-04 server-descriptors-2023-04 userstats-relay-country.csv tmodel-ccs2018.github.io --onionperf_data_path onionperf-2023-04 --bandwidth_data_path bandwidth-2023-04.csv --geoip_path ~/tor/src/config/geoip
 cd /part/simulation
-tornettools generate data/relayinfo_staging_2023-04-01--2023-04-30.json data/userinfo_staging_2023-04-01--2023-04-30.json data/networkinfo_staging.gml data/tmodel-ccs2018.github.io -r -e BW,CIRC,STREAM --server_scale 0.5 --network_scale 0.005 --prefix sim_300_4
-python3 patch.py -s sim_300_4 -c 300 -d 4
-zip -r sim_300_4.zip sim_300_4
+tornettools generate data/relayinfo_staging_2023-04-01--2023-04-30.json data/userinfo_staging_2023-04-01--2023-04-30.json data/networkinfo_staging.gml data/tmodel-ccs2018.github.io -r -e BW,CIRC,STREAM --server_scale 0.5 --network_scale 0.005 --prefix sim_300_2
+python3 patch.py -s sim_300_2 -c 300 -d 2
+zip -r sim_300_2.zip sim_300_2
 
 # Create a copy of the simulation for the normal simulation
-cp -r sim_300_4 ./sim_300_4_n
-echo "EnforceDelayPolicy 1" >> sim_300_4_n/conf/tor.client.torrc
-echo "DelayMode 3" >> sim_300_4_n/conf/tor.client.torrc
-echo "DelayParam1 0.5e5" >> sim_300_4_n/conf/tor.client.torrc
-echo "DelayParam2 0.12e5" >> sim_300_4_n/conf/tor.client.torrc
-echo "DelayMax 1e5" >> sim_300_4_n/conf/tor.client.torrc
+cp -r sim_300_2 ./sim_300_2_n
+echo "EnforceDelayPolicy 1" >> sim_300_2_n/conf/tor.client.torrc
+echo "DelayMode 3" >> sim_300_2_n/conf/tor.client.torrc
+echo "DelayParam1 0.5e5" >> sim_300_2_n/conf/tor.client.torrc
+echo "DelayParam2 0.12e5" >> sim_300_2_n/conf/tor.client.torrc
+echo "DelayMax 1e5" >> sim_300_2_n/conf/tor.client.torrc
+
+# Create a copy of the simulation for the uniform simulation
+cp -r sim_300_2 ./sim_300_2_u
+echo "EnforceDelayPolicy 1" >> sim_300_2_u/conf/tor.client.torrc
+echo "DelayMode 2" >> sim_300_2_u/conf/tor.client.torrc
+echo "DelayParam1 0.0" >> sim_300_2_u/conf/tor.client.torrc
+echo "DelayParam2 0.5e5" >> sim_300_2_u/conf/tor.client.torrc
+echo "DelayMax 0.5e5" >> sim_300_2_u/conf/tor.client.torrc
+
+# Create a copy of the simulation for the lognormal simulation
+cp -r sim_300_2 ./sim_300_2_l
+echo "EnforceDelayPolicy 1" >> sim_300_2_l/conf/tor.client.torrc
+echo "DelayMode 4" >> sim_300_2_l/conf/tor.client.torrc
+echo "DelayParam1 -1.5e5" >> sim_300_2_l/conf/tor.client.torrc
+echo "DelayParam2 0.5e5" >> sim_300_2_l/conf/tor.client.torrc
+echo "DelayMax 1e5" >> sim_300_2_l/conf/tor.client.torrc
