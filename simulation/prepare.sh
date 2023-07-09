0a00f6271c6150a00585854623ec303e47045c77
Afonso Gomes <afonso.gomes@tecnico.ulisboa.pt>
2023-07-09 15:46:09 +0100

    Changed prepare.sh to do minimal sim

--- simulation/prepare.sh
+++ simulation/prepare.sh
@@ -9,7 +9,7 @@ cd /part/simulation/data
 tornettools stage consensuses-2023-04 server-descriptors-2023-04 userstats-relay-country.csv tmodel-ccs2018.github.io --onionperf_data_path onionperf-2023-04 --bandwidth_data_path bandwidth-2023-04.csv --geoip_path ~/tor/src/config/geoip
 cd /part/simulation
 tornettools generate data/relayinfo_staging_2023-04-01--2023-04-30.json data/userinfo_staging_2023-04-01--2023-04-30.json data/networkinfo_staging.gml data/tmodel-ccs2018.github.io -r -e BW,CIRC,STREAM --server_scale 0.5 --network_scale 0.005 --prefix sim_300_2
-python3 patch.py -s sim_300_2 -c 300 -d 2
+python3 patch.py -s sim_300_2 -c 300 -d 2 -m
 zip -r sim_300_2.zip sim_300_2
 
 # Create a copy of the simulation for the normal simulation
