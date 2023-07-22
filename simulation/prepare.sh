e3f72e8964e92d1aa68e45e18673e3e2fa4be11d
Afonso Gomes <afonsopamg@gmail.com>
2023-07-22 17:47:11 +0100

    fixed $HOME unresolved

--- simulation/prepare.sh
+++ simulation/prepare.sh
@@ -6,7 +6,7 @@ cp -r /root/rendezmix/simulation /part
 
 # Generate the simulation configuration files
 cd /part/simulation/data
-tornettools stage consensuses-2023-04 server-descriptors-2023-04 userstats-relay-country.csv tmodel-ccs2018.github.io --onionperf_data_path onionperf-2023-04 --bandwidth_data_path bandwidth-2023-04.csv --geoip_path ~/tor/src/config/geoip
+tornettools stage consensuses-2023-04 server-descriptors-2023-04 userstats-relay-country.csv tmodel-ccs2018.github.io --onionperf_data_path onionperf-2023-04 --bandwidth_data_path bandwidth-2023-04.csv --geoip_path /root/tor/src/config/geoip
 cd /part/simulation
 tornettools generate data/relayinfo_staging_2023-04-01--2023-04-30.json data/userinfo_staging_2023-04-01--2023-04-30.json data/networkinfo_staging.gml data/tmodel-ccs2018.github.io -r -e BW,CIRC,STREAM --server_scale 0.5 --network_scale 0.005 --prefix sim_300_2
 python3 patch.py -s sim_300_2 -c 300 -d 2
