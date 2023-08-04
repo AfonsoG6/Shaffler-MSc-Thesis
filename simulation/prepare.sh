702eb5156d9ce9510714d2815ce6ace1ecd96c19
Afonso Gomes <afonso.gomes@tecnico.ulisboa.pt>
2023-08-04 16:11:28 +0100

    fixed small issue

--- simulation/prepare.sh
+++ simulation/prepare.sh
@@ -8,5 +8,5 @@ cd /part/cover
 # Generate the simulation configuration files
 cd /part/cover
 cp -r /part/simulation/data .
-tornettools generate data/relayinfo_staging_2023-04-01--2023-04-30.json data/userinfo_staging_2023-04-01--2023-04-30.json data/networkinfo_staging.gml data/tmodel-ccs2018.github.io -r -e BW,CIRC,STREAM --server_scale 0.5 --network_scale 0.005 --prefix sim_300_2
+tornettools generate data/relayinfo_staging_2023-04-01--2023-04-30.json data/userinfo_staging_2023-04-01--2023-04-30.json data/networkinfo_staging.gml data/tmodel-ccs2018.github.io -r -e BW,CIRC,STREAM --server_scale 0.5 --network_scale 0.005 --prefix sim_200_2
 python3 patch.py -s sim_200_2 -c 200 -d 2
