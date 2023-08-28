433b5b1ec17ee680e4c7fed4f1d8bd58ee35babd
Afonso Gomes <afonsopamg@gmail.com>
2023-08-28 23:10:22 +0100

    Added better logs and commented useless code

--- simulation/templates/customclient/traffic_gen/cover_client/client.py
+++ simulation/templates/customclient/traffic_gen/cover_client/client.py
@@ -4,29 +4,34 @@ import time
 from requests_html import HTMLSession, HTMLResponse
 from requests import Response
 from fake_useragent import UserAgent
-from threading import Thread
+from threading import Thread, Condition
 from random import random
+from datetime import datetime
 
+def log(context: str, message: str) -> None:
+    print(f"{datetime.now().strftime('%b %d %H:%M:%S.%f')} [LOG,{context}] {message}")
 
 def do_request(
-    session: HTMLSession, address: str, headers: dict, timeout: float, socks5: str
+    session: HTMLSession, address: str, headers: dict, timeout: float, socks5: str, thread_id: int, request_id: int
 ):
-    print(f"[COVER] Sending request to {address}")
+    log(f"THREAD#{thread_id}", f"Sending request #{request_id} to {address}")
     res: Response = session.get(
         address,
         headers=headers,
         timeout=timeout,
         proxies={"http": socks5, "https": socks5},
     )
-    print(f"[COVER] Received response: {res.status_code}")
+    log(f"THREAD#{thread_id}", f"Received response for request #{request_id}: {res.status_code}")
+    """
     if not isinstance(res, HTMLResponse):
-        print("[COVER] Received non-HTML response")
+        log(f"THREAD#{thread_id}", "Received non-HTML response")
         return
     hres: HTMLResponse = res
     hres.html.render(timeout=timeout)
+    """
 
 
-def run_client(start_delay, configs):
+def run_client(start_delay, configs, thread_id):
     address = "http://" + configs["address"] + configs["endpoint"]
     base_delta = configs["delta"]
     deviation = configs["deviation"]
@@ -43,6 +48,7 @@ def run_client(start_delay, configs):
     time.sleep(start_delay)
 
     fails = 0
+    counter = 1
     with HTMLSession() as session:
         while True:
             try:
@@ -51,18 +57,19 @@ def run_client(start_delay, configs):
                 target_delta = base_delta + (random() * 2 - 1) * deviation
                 # Timeout is minimum of base_timeout and delta
                 timeout = base_timeout if base_timeout < target_delta else target_delta
-                print(f"[COVER] Sending request to {address}")
-                do_request(session, address, headers, timeout, socks5)
+                do_request(session, address, headers, timeout, socks5, thread_id, counter)
                 fails = 0
+                counter += 1
                 delta = time.time() - t0
                 remaining = target_delta - delta
                 if remaining > 0:
+                    log(f"THREAD#{thread_id}", f"Sleeping for {remaining} seconds of a total of {target_delta} seconds")
                     time.sleep(remaining)
             except Exception as e:
-                print(f"[COVER] Exception: {e}")
+                log(f"THREAD#{thread_id}", f"Exception: {e}")
                 fails += 1
                 if tolerated_fails >= 0 and fails > tolerated_fails:
-                    print("[COVER] Exceeded fail limit... Stopping Client Thread")
+                    log(f"THREAD#{thread_id}", "Exceeded fail limit... Stopping Client Thread")
                     return
 
 
@@ -80,15 +87,15 @@ def main(configs):
     threads = []
     for i in range(num_threads):
         start_delay = i * delta / num_threads
-        client_thread = Thread(target=run_client, args=[start_delay, configs])
+        client_thread = Thread(target=run_client, args=[start_delay, configs, i+1])
         threads.append(client_thread)
         client_thread.daemon = True
         client_thread.start()
-        print(f"[COVER] Launched new client thread with start delay {start_delay}")
+        log("MAIN", f"Launched new client thread with start delay {start_delay}")
     for thread in threads:
         thread.join()
-        print("[COVER] Client thread has been joined")
-    print("[COVER] All client threads have been joined... Exiting")
+        log("MAIN", "Client thread has been joined")
+    log("MAIN", "All client threads have been joined... Exiting")
 
 
 if __name__ == "__main__":
